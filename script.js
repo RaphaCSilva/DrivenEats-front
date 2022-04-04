79c36d831ac9957663b2106c96881a6a33038bb5
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-04-04 06:27:58 -0300

    checkado_faltavariedade

--- script.js
+++ script.js
@@ -13,48 +13,63 @@ return document.querySelector(`.${elemento}`)
 }
 function marcarA(opcaoclicada) {
   const opcaoA = document.querySelector(".opcaomarcadaA");
+  const vezinho = document.querySelector(".iconeA");
+  if (vezinho !== null) {
+    vezinho.classList.toggle("iconeA");
+  }
   if (opcaoA !== null) {
     opcaoA.classList.toggle("opcaomarcadaA");
   }
   opcaoclicada.classList.add("opcaomarcadaA");
+  opcaoclicada.children[4].classList.add("iconeA");
   nomeA = (opcaoclicada.children[1].innerHTML);
   precoA = (Number(opcaoclicada.querySelector("span").innerHTML));
   contadorA = 1;
   somatotal = contadorA + contadorB + contadorC;
   if (somatotal === 3){
-    document.querySelector(".caixinha").style.background = "green";
+    document.querySelector(".caixinha").style.background = "#32B72F";
     document.querySelector(".textosemselecao").classList.add("none");
     document.querySelector(".textocomselecao").classList.remove("textocomselecao");
   }
 }  
 function marcarB(opcaoclicada) {
   const opcaoB = document.querySelector(".opcaomarcadaB");
+  const vezinho = document.querySelector(".iconeB");
+  if (vezinho !== null) {
+    vezinho.classList.toggle("iconeB");
+  }
   if (opcaoB !== null) {
     opcaoB.classList.toggle("opcaomarcadaB");
   }
   opcaoclicada.classList.add("opcaomarcadaB");
+  opcaoclicada.children[4].classList.add("iconeB");
   nomeB = (opcaoclicada.children[1].innerHTML);
   precoB = (Number(opcaoclicada.querySelector("span").innerHTML));
   contadorB = 1;
   somatotal = contadorA + contadorB + contadorC;
   if (somatotal === 3){
-    document.querySelector(".caixinha").style.background = "green";
+    document.querySelector(".caixinha").style.background = "#32B72F";
     document.querySelector(".textosemselecao").classList.add("none");
     document.querySelector(".textocomselecao").classList.remove("textocomselecao");
   }
 }
 function marcarC(opcaoclicada) {
   const opcaoC = document.querySelector(".opcaomarcadaC");
+  const vezinho = document.querySelector(".iconeC");
+  if (vezinho !== null) {
+    vezinho.classList.toggle("iconeC");
+  }
   if (opcaoC !== null) {
     opcaoC.classList.toggle("opcaomarcadaC");
   }
   opcaoclicada.classList.add("opcaomarcadaC");
+  opcaoclicada.children[4].classList.add("iconeC");
   nomeC = (opcaoclicada.children[1].innerHTML);
   precoC = (Number(opcaoclicada.querySelector("span").innerHTML));
   contadorC = 1;
   somatotal = contadorA + contadorB + contadorC;
   if (somatotal === 3){
-    document.querySelector(".caixinha").style.background = "green";
+    document.querySelector(".caixinha").style.background = "#32B72F";
     document.querySelector(".textosemselecao").classList.add("none");
     document.querySelector(".textocomselecao").classList.remove("textocomselecao");
   }
